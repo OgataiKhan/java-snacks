191d1ebb1bc1a7bbda40c7d727968c38a897d6f3
OgataiKhan <badellino.kevin@gmail.com>
2024-05-16 14:23:51 +0200

    feat: snack 2

---
+++ src/org/snacks/Snack2.java
@@ -0,0 +1,48 @@
+package org.snacks;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Snack2 {
+    public static void main(String[] args) {
+        String[] firstNames = {
+                "James",
+                "Emily",
+                "Michael",
+                "Sarah",
+                "David",
+                "Ashley",
+                "John",
+                "Jessica",
+                "Matthew",
+                "Amanda"
+        };
+
+        String[] lastNames = {
+                "Smith",
+                "Johnson",
+                "Williams",
+                "Brown",
+                "Jones",
+                "Miller",
+                "Davis",
+                "Garcia",
+                "Wilson",
+                "Martinez"
+        };
+
+        // Create instance of Random class
+        Random rand = new Random();
+
+        int firstNameIndex, lastNameIndex;
+        String[] fullNames = new String[10];
+
+        for (int i = 0; i < fullNames.length; i++) {
+            firstNameIndex = rand.nextInt(10);
+            lastNameIndex = rand.nextInt(10);
+            fullNames[i] = firstNames[firstNameIndex] + " " + lastNames[lastNameIndex];
+        }
+
+        System.out.println(Arrays.toString(fullNames));
+    }
+}
